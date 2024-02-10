abcd4b8080f34e094fc8c2bacafa9af24cc07291
bhoomi <chouhanriya1724@gmail.com>
2024-02-10 22:35:28 +0530

    create user function working

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -14,6 +14,11 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { SignupValidation } from "@/lib/validation";
+import Loader from "@/components/shared/Loader";
+import { Link } from "react-router-dom";
+import { createUserAccount } from "@/lib/appwrite/api";
+
+
 
 const SignupForm = () => {
   // 1. Define your form.
@@ -28,11 +33,12 @@ const SignupForm = () => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+   const newUser = await createUserAccount(values);
+    console.log(newUser);
   }
+
+  const isLoading = false;
 
   return (
     <Form {...form}>
@@ -124,7 +130,12 @@ const SignupForm = () => {
               </FormItem>
             )}
           />
-          <Button type="submit" className="shad-button_primary mt-4">Sign Up</Button>
+          <Button type="submit" className="shad-button_primary mt-4">
+            {isLoading?(<div className="flex-center gap-2">
+              <Loader/>
+            </div>):("Sign Up")}
+          </Button>
+          <p className="small-regular text-light-2 text-center mt-2">Already have an account? <Link className="text-primary-500 small-semibold ml-1" to={"/sign-in"}>Sign in</Link></p>
         </form>
       </div>
     </Form>
